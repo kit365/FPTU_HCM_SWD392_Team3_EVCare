0249e9649dd0f95f020e84e5c83afcd803d7332f
Duc Long <longntdse181516@fpt.edu.vn>
2025-10-02 10:24:43 +0700

    [REFACTOR] tạo  middle ware + logic xử lí token

---
+++ FE/src/hooks/useAuthClient.ts
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import type { LoginRequest } from "../type/auth";
+import type { RegisterUserRequest } from "../type/auth";
+import { authService } from "../service/authService";
+import { notify } from "../components/admin/common/Toast";
+import { AxiosError } from "axios";
+import { useAuthContext } from '../context/useAuthContext.tsx';
+
+
+export function useAuth() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { refreshUser } = useAuthContext();
+  const navigate = useNavigate();
+
+  const login = async (data: LoginRequest) => {
+    setIsLoading(true);
+    try {
+      const response = await authService.login(data);
+      //   if (!res?.token) {
+      //     showErrorToast("Đăng nhập thất bại: Không nhận được token");
+      //     return null;
+      //   }
+      if (response?.data.success === true) {
+        notify.success(response?.data.message || "Đăng nhập thành công")
+        //lưu accesstoken local storage
+        //localStorage.setItem('user_info', JSON.stringify(response.data.user));
+        await refreshUser(); // Gọi API lấy user và set vào context      
+        navigate("/");
+      } else {
+        console.log(response?.data.message);
+        notify.error(response?.data.message || "Đăng nhập thất bại");
+      }
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response && axiosError.response.data) {
+        notify.error((axiosError.response.data as any).message || "Đăng nhập thất bại");
+      } else {
+        notify.error(axiosError?.message || "Đăng nhập thất bại");
+      }
+
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const registerUser = async (data: RegisterUserRequest) => {
+    setIsLoading(true);
+    try {
+      const response = await authService.registerUser(data);
+      if (response?.data?.success === true) {
+        notify.success(response?.data?.message || "Đăng ký thành công");
+      } else {
+        notify.error(response?.data?.message || "Đăng ký thất bại");
+      }
+      return response;
+    } catch (error) {
+      notify.error("Đăng ký tài khoản thất bại");
+      throw error;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+
+  return {
+    login,
+    registerUser,
+    isLoading,
+  };
+}
--- FE/src/type/auth.ts
+++ FE/src/type/auth.ts
@@ -9,17 +9,17 @@ export interface LoginResponse {
     authenticated: boolean;
 }
 
-    export interface RegisterUserRequest {
-        username : string;
-        password : string;
-        email : string;
-        numberPhone: string;
-    }
+export interface RegisterUserRequest {
+    username: string;
+    password: string;
+    email: string;
+    numberPhone: string;
+}
 
 
 
-    export interface RegisterUserResponse {
-       userId: string;
-       email: string;
-       token: string;
-    }
+export interface RegisterUserResponse {
+    userId: string;
+    email: string;
+    token: string;
+}
