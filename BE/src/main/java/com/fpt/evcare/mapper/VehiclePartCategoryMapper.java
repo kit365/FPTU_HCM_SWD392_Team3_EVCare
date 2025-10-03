5d603597bb577b089bece6a2fc6975dbf08abdb0
Phu Dep Trai <phujason1992@gmail.com>
2025-10-03 15:48:26 +0700

    [FIX] Updating logic in part category entity

--- BE/src/main/java/com/fpt/evcare/mapper/VehiclePartCategoryMapper.java
+++ BE/src/main/java/com/fpt/evcare/mapper/VehiclePartCategoryMapper.java
@@ -8,21 +8,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface VehiclePartCategoryMapper {
 
     @Mapping(target = "vehiclePartCategoryId", ignore = true)
+    @Mapping(target = "vehicleParts", ignore = true)
     @Mapping(target = "search", ignore = true)
     VehiclePartCategoryEntity toEntity(CreationVehiclePartCategoryRequest creationVehiclePartCategoryRequest);
 
     VehiclePartCategoryResponse toResponse(VehiclePartCategoryEntity vehiclePartCategoryEntity);
 
-    List<VehiclePartCategoryResponse> toResponseList(List<VehiclePartCategoryEntity> vehiclePartCategoryEntities);
-
     @Mapping(target = "vehiclePartCategoryId", ignore = true)
-    @Mapping(target = "partCategoryName",  ignore = true)
+    @Mapping(target = "vehicleParts", ignore = true)
     @Mapping(target = "search", ignore = true)
+    @Mapping(target = "partCategoryName", ignore = true)
     void toUpdate(UpdationVehiclePartCategoryRequest updationVehiclePartCategoryRequest, @MappingTarget VehiclePartCategoryEntity vehiclePartCategoryEntity);
 }
