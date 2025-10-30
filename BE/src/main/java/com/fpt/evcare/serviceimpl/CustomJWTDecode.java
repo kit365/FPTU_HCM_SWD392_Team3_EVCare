4b514c46e6dbec43959cf4b305d0a59432a98ad7
kit365 <kietops365@gmail.com>
2025-10-30 20:03:28 +0700

    [FEAT] Update JWT configuration and refactor CustomJWTDecode for improved key management

--- BE/src/main/java/com/fpt/evcare/serviceimpl/CustomJWTDecode.java
+++ BE/src/main/java/com/fpt/evcare/serviceimpl/CustomJWTDecode.java
@@ -1,8 +1,8 @@
 package com.fpt.evcare.serviceimpl;
 import com.fpt.evcare.exception.JWTInitializationException;
 import com.nimbusds.jose.JOSEException;
-import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.nimbusds.jose.crypto.MACSigner;
 import java.nio.charset.StandardCharsets;
@@ -11,16 +11,15 @@ import java.nio.charset.StandardCharsets;
 @Service
 public class CustomJWTDecode {
     private final MACSigner macSigner;
-    public CustomJWTDecode() {
+    
+    public CustomJWTDecode(@Value("${jwt.signer-key}") String jwtSignerKey) {
         try {
-            Dotenv dotenv = Dotenv.load();
-            String key = dotenv.get("JWT_SIGNER_KEY");
-            if (key == null || key.isBlank()) {
-                throw new JWTInitializationException("JWT_SIGNER_KEY is missing in .env");
+            if (jwtSignerKey == null || jwtSignerKey.isBlank()) {
+                throw new JWTInitializationException("JWT_SIGNER_KEY is missing in application properties");
             }
-            this.macSigner = new MACSigner(key.getBytes(StandardCharsets.UTF_8));
+            this.macSigner = new MACSigner(jwtSignerKey.getBytes(StandardCharsets.UTF_8));
         } catch (JOSEException e) {
-            throw new JWTInitializationException("Failed to initialize MACSigner");
+            throw new JWTInitializationException("Failed to initialize MACSigner: " + e.getMessage());
         }
     }
 
